fcb4d17b15c977fd349e0d42a0c8e612e59ebd0f
shubham97190 <shubham.prajapati@dotsquares.com>
2019-03-18 19:46:57 +0530

    18 march

--- magazine/urls.py
+++ magazine/urls.py
@@ -15,13 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index,music, lifestyle, fashion,  news, business,sport
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',index)
+    path('',index),
+    path('music',music),
+    path('news',news),
+    path('business',business),
+    path('sport',sport),
+    path('lifestyle',lifestyle),
+    path('fashion',fashion),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- magazine/views.py
+++ magazine/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse
-import psycopg2
 from article.models import Article,MagazineCategory
+import psycopg2
 
 # Create your views here.
 def index(request):
@@ -19,3 +19,98 @@ def index(request):
     }
     return render(request,'index.html',context)
 
+def news(request):
+    article_qurey_set=""
+    magazine_category_qurey_set=""
+    try:
+        magazine_category_qurey_set = MagazineCategory.objects.all()
+        article_qurey_set = Article.objects.all()[:4]
+        
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    
+    context = {'navbar':magazine_category_qurey_set,
+                'trading_post':article_qurey_set     
+    
+    }
+    return render(request,'post.html',context)
+
+def business(request):
+    article_qurey_set=""
+    magazine_category_qurey_set=""
+    try:
+        magazine_category_qurey_set = MagazineCategory.objects.all()
+        article_qurey_set = Article.objects.all()[:4]
+        
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    
+    context = {'navbar':magazine_category_qurey_set,
+                'trading_post':article_qurey_set     
+    
+    }
+    return render(request,'post.html',context)
+
+def sport(request):
+    article_qurey_set=""
+    magazine_category_qurey_set=""
+    try:
+        magazine_category_qurey_set = MagazineCategory.objects.all()
+        article_qurey_set = Article.objects.all()[:4]
+        
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    
+    context = {'navbar':magazine_category_qurey_set,
+                'trading_post':article_qurey_set     
+    
+    }
+    return render(request,'post.html',context)
+
+def lifestyle(request):
+    article_qurey_set=""
+    magazine_category_qurey_set=""
+    try:
+        magazine_category_qurey_set = MagazineCategory.objects.all()
+        article_qurey_set = Article.objects.all()[:4]
+        
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    
+    context = {'navbar':magazine_category_qurey_set,
+                'trading_post':article_qurey_set     
+    
+    }
+    return render(request,'post.html',context)
+
+def fashion(request):
+    article_qurey_set=""
+    magazine_category_qurey_set=""
+    try:
+        magazine_category_qurey_set = MagazineCategory.objects.all()
+        article_qurey_set = Article.objects.all()[:4]
+        
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    
+    context = {'navbar':magazine_category_qurey_set,
+                'trading_post':article_qurey_set     
+    
+    }
+    return render(request,'post.html',context)
+
+def music(request):
+    article_qurey_set=""
+    magazine_category_qurey_set=""
+    try:
+        magazine_category_qurey_set = MagazineCategory.objects.all()
+        article_qurey_set = Article.objects.all()[:4]
+        
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    
+    context = {'navbar':magazine_category_qurey_set,
+                'trading_post':article_qurey_set     
+    
+    }
+    return render(request,'post.html',context)
